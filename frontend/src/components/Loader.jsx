d32098888dc0543a1a1a7276c3e871ce05084b7f
Luca Almiron <lucadev.arg@gmail.com>
2024-09-08 23:12:59 -0300

    update 1.9.1

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,6 +1,6 @@
 const Loader = () => {
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center min-h-screen">
       <div className="loader border-[16px] border-white dark:border-black rounded-full w-[120px] h-[120px] border-t-sky-500 dark:border-t-sky-500"></div>
     </div>
   );
