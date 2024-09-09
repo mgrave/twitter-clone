b67954377c79dee20db7a3d12ea333644a87c047
Luca Almiron <lucadev.arg@gmail.com>
2024-09-09 00:24:59 -0300

    update 1.99

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -2,7 +2,9 @@
 const Loader = ({ feed }) => {
   return (
     <div
-      className={`flex justify-center items-center ${!feed && "min-h-screen"}`}
+      className={`flex justify-center items-center ${
+        !feed ? "min-h-screen" : "h-full"
+      }`}
     >
       <div className="loader border-[16px] border-white dark:border-black rounded-full w-[120px] h-[120px] border-t-sky-500 dark:border-t-sky-500"></div>
     </div>
