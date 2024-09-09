70c0ff67b720b4e443db0f2ca2855446cc578c4f
Luca Almiron <lucadev.arg@gmail.com>
2024-09-08 21:31:45 -0300

    update

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,12 @@ const PORT = process.env.PORT || 5000;
 await connectDB();
 await connectGridFS();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://twitter-clone-pi-puce.vercel.app"],
+    credentials: true,
+  })
+);
 app.use(morgan("dev"));
 app.use(express.json());
 
